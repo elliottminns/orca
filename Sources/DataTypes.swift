f2ac205157b8ffcfffd882a2e2efaf5ca451a739
Elliott Minns <elliott.minns@me.com>
2016-02-14 22:07:51 +0000

    Added in string conversions to DataType protocol

--- Sources/DataTypes.swift
+++ Sources/DataTypes.swift
@@ -6,28 +6,66 @@ public enum ValueType {
     case Int
     case Double
     case Float
+    case Bool
 }
 
 public protocol DataType {
-
+    func toString() -> String
+    static func fromString(string: String) -> Self
 }
 
 extension String: DataType {
 
+    public func toString() -> String {
+        return self
+    }
+
+    public static func fromString(string: String) -> String {
+        return string
+    }
 }
 
 extension Int: DataType {
 
+    public func toString() -> String {
+        return "\(self)"
+    }
+
+    static public func fromString(string: String) -> Int {
+        return Int(string) ?? 0
+    }
 }
 
 extension Double: DataType {
 
+    public func toString() -> String {
+        return "\(self)"
+    }
+
+    static public func fromString(string: String) -> Double {
+        return Double(string) ?? 0
+    }
 }
 
 extension Float: DataType {
 
+    public func toString() -> String {
+        return "\(self)"
+    }
+
+    static public func fromString(string: String) -> Float {
+        return Float(string) ?? 0
+    }
 }
 
 extension Bool: DataType {
-    
+
+    public func toString() -> String {
+        return self == true ? "1" : "0"
+    }
+
+    static public func fromString(string: String) -> Bool {
+        return string == "1"
+    }
+
 }
