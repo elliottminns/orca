fef5cf8e39063dbe4eaa3ea038253040550d693b
Elliott Minns <elliott.minns@me.com>
2016-02-14 23:15:27 +0000

    Added in measures to overcome SQL non type

--- Sources/DataTypes.swift
+++ Sources/DataTypes.swift
@@ -9,32 +9,83 @@ public enum ValueType {
     case Bool
 }
 
-public protocol DataType {
-
-}
-
-extension DataType {
-    var type: String {
-        return "\(self.dynamicType)"
+extension Orca {
+    static public var supportedTypes: [DataType.Type] {
+        return [String.self, Int.self, Double.self, Float.self, Bool.self]
     }
 }
 
-extension String: DataType {
+public protocol DataType {
+    static func fromString(string: String) -> Self?
+    func toString() -> String
+}
 
+extension DataType {
+
+    public var typeString: String {
+        return "\(self.dynamicType)"
+    }
+
+    static func dataTypeFromTypeString(string: String) -> DataType.Type? {
+
+        for type in Orca.supportedTypes {
+
+            if "\(type)" == string {
+                return type
+            }
+        }
+
+        return nil
+    }
+
+}
+
+extension String: DataType {
+    static public func fromString(string: String) -> String? {
+        return string
+    }
+
+    public func toString() -> String {
+        return "\(self)"
+    }
 }
 
 extension Int: DataType {
+    static public func fromString(string: String) -> Int? {
+        return Int(string)
+    }
 
+    public func toString() -> String {
+        return "\(self)"
+    }
 }
 
 extension Double: DataType {
+    static public func fromString(string: String) -> Double? {
+        return Double(string)
+    }
 
+    public func toString() -> String {
+        return "\(self)"
+    }
 }
 
 extension Float: DataType {
+    static public func fromString(string: String) -> Float? {
+        return Float(string)
+    }
 
+    public func toString() -> String {
+        return "\(self)"
+    }
 }
 
 extension Bool: DataType {
+    static public func fromString(string: String) -> Bool? {
+        return string == "1"
+    }
 
+    public func toString() -> String {
+        return self ? "1" : "0"
+    }
 }
