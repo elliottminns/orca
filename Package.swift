fcfb3e924159d229cc52ef40c20bad7f014f538d
Elliott Minns <elliott.minns@me.com>
2016-04-01 13:15:51 +0100

    removed bson

--- Package.swift
+++ Package.swift
@@ -4,8 +4,6 @@ let package = Package(
     name: "Orca",
     dependencies: [
         .Package(url: "https://github.com/elliottminns/echo.git",
-            majorVersion: 0),
-        .Package(url: "../bson-osx",
-                 majorVersion: 0)
+            majorVersion: 0)
     ]
 )
